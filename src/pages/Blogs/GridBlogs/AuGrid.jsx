9f621731b9e2bce988a06deba9dddadbd7c100fe
kuwarp <purushottam.yadav@imsplglobal.com>
2023-04-04 13:29:01 +0530

    UPPER-GRID

--- src/pages/Blogs/GridBlogs/AuGrid.jsx
+++ src/pages/Blogs/GridBlogs/AuGrid.jsx
@@ -267,6 +267,12 @@ const posts = [
      
     </div> 
     
+    {/* Navbar Grid End */}
+        <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32  lg:px-8">
+
+        
+     
+    
              <div className="mx-auto max-w-2xl lg:mx-0">
             <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Credit Cards Blogs</h2>
             <p className="mt-2 text-lg leading-8 text-gray-600">
