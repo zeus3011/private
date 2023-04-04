a44267f1c5fb961c4a2d45ecd9fe817a842d606d
kuwarp <purushottam.yadav@imsplglobal.com>
2023-04-04 13:38:57 +0530

    Au-small

--- src/pages/Blogs/GridBlogs/AuGrid.jsx
+++ src/pages/Blogs/GridBlogs/AuGrid.jsx
@@ -267,6 +267,41 @@ const posts = [
      
     </div> 
     
+
+    <div>
+            <label htmlFor="last-name" className="block text-lg font-semibold leading-6 text-gray-900">
+          Categories
+            </label>
+            <div className="mt-2.5">
+              {/* <input
+                type="list"
+                name="last-name"
+                id="last-name"
+                autoComplete="family-name"
+                className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              /> */}
+
+<select id="cars"  className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            name="carlist" form="carform">
+  <option value="volvo">
+    
+    <ul>
+      Loans
+ 
+       
+    </ul>
+  </option>
+  
+  <option value="saab">Credit cards</option>
+  <option value="opel">Credit Score</option>
+  <option value="opel">All need of Credit Cards</option>
+  <option value="opel">All About Banking</option>
+  
+  <option value="opel">Others</option>
+
+</select>
+            </div>
+          </div>
     {/* Navbar Grid End */}
         <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32  lg:px-8">
 
